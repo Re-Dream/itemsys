755b9bdd1daaf6291e3cadd89aca872e47acac84
TwentySix <refertrionjared1@gmail.com>
2018-10-04 07:59:34 -0400

    Fix

--- lua/autorun/itemsys/npcs/bartender.lua
+++ lua/autorun/itemsys/npcs/bartender.lua
@@ -12,7 +12,6 @@ rd_items.NPCShops[tag] = {
 }
 
 rd_items.NPCSpawns["redream_waterlands_3"][tag] = {
-        pos = Vector(5952.4145507812, 7343.1591796875, -87.96875),
-        ang = Angle(0,-90,0)
-    }
+    pos = Vector(5952.4145507812, 7343.1591796875, -87.96875),
+    ang = Angle(0,-90,0)
 }
