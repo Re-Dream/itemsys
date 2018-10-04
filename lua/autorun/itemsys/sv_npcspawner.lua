587d324e31d71ad645c8688e46940d489f018998
Etothepowerof26 <refertrionjared1@gmail.com>
2018-10-04 07:50:51 -0400

    Update sv_npcspawner.lua

--- lua/autorun/itemsys/sv_npcspawner.lua
+++ lua/autorun/itemsys/sv_npcspawner.lua
@@ -2,7 +2,7 @@ rd_items.CreateNPCs = function()
     local s = rd_items.NPCSpawns[game.GetMap()]
     if s then
         for i,v in pairs(s) do
-            local e = ents.Create("npc_redream_shopkeeper")
+            local e = ents.Create("redream_shopkeeper")
             e:SetPos(v.pos)
             e:SetAngles(v.ang)
             e.Shop = rd_items.NPCShops[i]
