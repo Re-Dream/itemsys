885f0b60b71844e80ceadad103b4835fe91a5805
Etothepowerof26 <refertrionjared1@gmail.com>
2018-10-04 07:18:07 -0400

    Update init.lua

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -1,7 +1,27 @@
 --use pdata instead of netdata if used on different than redream
-
 local PLAYER = FindMetaTable("Player")
-if not PLAYER.GetNetData or not PLAYER.SetNetData then
+if not PLAYER.GetNetData then
 	PLAYER.GetNetData = PLAYER.GetPData
-	PLAYER.SetNetData = PLAYER.SetPData
+end
+
+if SERVER then
+	if not PLAYER.SetNetData then
+		PLAYER.SetNetData = PLAYER.SetPData
+	end
+	
+	local function includeCSLuaFile(x)
+		include(x)
+		AddCSLuaFile(x)
+	end
+	
+	includeCSLuaFile("itemsys/sh_coins.lua")
+	includeCSLuaFile("itemsys/sh_itemsys.lua")
+	include("itemsys/sv_npcspawner.lua")
+	
+	--todo: init all npcs
+end
+
+if CLIENT then
+	include("itemsys/sh_coins.lua")
+	include("itemsys/sh_itemsys.lua")
 end
